4abeb8dbbe015a2c2d53bf5bcbffe5e4be936f8c
mas <m.yoshida553@gmail.com>
2025-02-07 19:47:18 +0900

    first commit

--- PhotoApp/ContentView.swift
+++ PhotoApp/ContentView.swift
@@ -1,24 +1,69 @@
-//
-//  ContentView.swift
-//  PhotoApp
-//
-//  Created by masato yoshida on 2025/02/07.
-//
-
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  @State var selectedPhotoItem: PhotosPickerItem? = nil
+  @State var image: Image? = nil
+
+  var body: some View {
+    VStack {
+      imageView
+
+      HStack {
+        Button("削除") {
+          image = nil
         }
-        .padding()
+        .modifier(ButtonModifier(backgroundColor: .red))
+
+        PhotosPicker(selection: $selectedPhotoItem, matching: .images) { // 画像を選びたいので.images
+          Text("写真を選択する") // ButtonをTextに変更
+            .modifier(ButtonModifier(backgroundColor: .green))
+        }
+      }
     }
+    .padding()
+    .onChange(of: selectedPhotoItem){
+      Task{
+        do{
+          image = try await selectedPhotoItem?.loadTransferable(type: Image.self)
+        }
+        catch{
+          print(error.localizedDescription)
+        }
+
+      }
+
+    }
+  }
+}
+
+extension ContentView {
+  var imageView: some View {
+    if let image {
+      image
+        .resizable()
+        .frame(width: 300, height: 300)
+    } else {
+      Image("noimage")
+        .resizable()
+        .frame(width: 300, height: 300)
+    }
+  }
+}
+
+struct ButtonModifier: ViewModifier {
+  let backgroundColor: Color
+  func body(content: Content) -> some View {
+    content
+      .padding()
+      .frame(minHeight: 50)
+      .background(backgroundColor)
+      .foregroundColor(.white)
+      .clipShape(RoundedRectangle(cornerRadius: 8))
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
+
