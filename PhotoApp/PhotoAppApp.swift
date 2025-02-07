84cf82ae9d92e636062e88dc1bea7032273b188b
mas <m.yoshida553@gmail.com>
2025-02-07 18:04:08 +0900

    Initial Commit

---
+++ PhotoApp/PhotoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoAppApp.swift
+//  PhotoApp
+//
+//  Created by masato yoshida on 2025/02/07.
+//
+
+import SwiftUI
+
+@main
+struct PhotoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
